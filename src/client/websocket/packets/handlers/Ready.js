2e140254417880e56ac8c3534743789f2f6fd614
GiorgioBrux <GiorgioBrux@users.noreply.github.com>
2020-09-14 19:43:15 +0200

    Update Ready.js

--- src/client/websocket/packets/handlers/Ready.js
+++ src/client/websocket/packets/handlers/Ready.js
@@ -38,8 +38,9 @@ class ReadyHandler extends AbstractHandler {
     if (data.notes) {
       for (const user of Object.keys(data.notes)) {
         let note = data.notes[user];
-        if (!note.length) note = null;
-
+        if(note != null)
+          if(note.lenght === 0) note = null;
+        
         client.user.notes.set(user, note);
       }
     }
