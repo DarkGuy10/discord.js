5d03d84e16c200ec233ad430afb5c140666b7177
GiorgioBrux <GiorgioBrux@users.noreply.github.com>
2021-01-23 11:49:58 +0100

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -85,8 +85,7 @@ exports.DefaultOptions = {
   },
 };
 
-exports.UserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 
-(KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36`;
+exports.UserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36`;
 
 exports.WSCodes = {
   1000: 'WS_CLOSE_REQUESTED',
